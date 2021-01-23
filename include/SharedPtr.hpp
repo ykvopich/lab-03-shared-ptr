d72ec8731c245fc656de14bb7c709032c1f36e48
ykvopich <kravchenko_vlad2001@bk.ru>
2021-01-24 00:16:05 +0300

    try7 #ifndef INCLUDE_HEADER_HPP_ #define INCLUDE_HEADER_HPP_
    #include <iostream>
    #include <atomic>
    #include <utility>
    
    using std::move;
    using std::atomic_uint;
    
    template <typename T>
    class SharedPtr {
     public:
     SharedPtr(): pointer(nullptr), counter(nullptr){}
    
      explicit SharedPtr(T* newPointer) {
        pointer=newPointer;
        if (newPointer == nullptr){
          counter = nullptr;
        } else {
          counter = new std::atomic_uint ;
          *counter=1;
        }
      };
    
      SharedPtr(const SharedPtr& sharedPtr){
        if (sharedPtr.pointer){
          pointer = sharedPtr.pointer;
          counter = sharedPtr.counter;
          if  (pointer){
            *counter = *counter+1;
          } else {
            counter = nullptr;
          }
        }
      };
    
      SharedPtr(SharedPtr&& sharedPtr) {
        pointer = nullptr;
        counter = nullptr;
        if (sharedPtr.pointer) {
          std::swap(pointer, sharedPtr.pointer);
          std::swap(counter, sharedPtr.counter);
        }
      };
    
      SharedPtr& operator=(const SharedPtr& sharedPtr){
        if (this != &sharedPtr) {
          pointer = sharedPtr.pointer;
          counter = sharedPtr.counter;
          if  (pointer){
            *counter = *counter+1;
          } else {
            counter = nullptr;
          }
        }
        return *this;
      };
    
      SharedPtr& operator=(SharedPtr&& sharedPtr) {
        if (this != &sharedPtr) {
          std::swap(pointer, sharedPtr.pointer);
          std::swap(counter, sharedPtr.counter);
        }
        return *this;
      };
    
      ~SharedPtr() {
        if (counter) {
          if (!--*counter) {
            delete pointer;
            delete counter;
          }
        }
      }
    
      operator bool() const{
        if(pointer)
          return true;
        else {
          return false;
        }
      }
    
      T& operator*() const {
          return *pointer;
      }
    
      T* operator->() const{
        return pointer;
      }
    
      T* get() {
        return pointer;
      }
    
      void reset() {
        if(*counter==1){
          delete pointer;
          delete counter;
        }else{
          *counter=*counter-1;
        }
        pointer=nullptr;
        counter=nullptr;
      }
    
      void reset(T* point){
        if(*counter==1){
          delete pointer;
          delete counter;
        }else {
          *counter = *counter - 1;
        }
        pointer=point;
        if(point==nullptr){
          counter=nullptr;
        }else{
          counter=new atomic_uint;
          *counter=1;
        }
      };
    
      void swap(SharedPtr& sharedPtr){
        T* tmpPointer = sharedPtr.pointer;
        sharedPtr.pointer = pointer;
        pointer = tmpPointer;
        atomic_uint* tmpCounter = sharedPtr.counter;
        sharedPtr.counter = counter;
        counter = tmpCounter;
      };
    
    [[nodiscard]] size_t use_count() const{
      if(counter){
        return *counter;
      }else{
        return 0;
      }
    }
    
     private:
      T* pointer;
      atomic_uint* counter;
    };
    
    #endif // INCLUDE_HEADER_HPP_

--- include/SharedPtr.hpp
+++ include/SharedPtr.hpp
@@ -3,7 +3,6 @@
 #ifndef INCLUDE_HEADER_HPP_
 #define INCLUDE_HEADER_HPP_
 
-
 #include <iostream>
 #include <atomic>
 #include <utility>
@@ -16,13 +15,12 @@ class SharedPtr {
  public:
   SharedPtr(): pointer(nullptr), counter(nullptr){}
 
-  explicit SharedPtr(T* newPointer){
+  explicit SharedPtr(T* newPointer) {
     pointer=newPointer;
-    if (newPointer==nullptr){
-      counter=nullptr;
-    }
-    else{
-      counter=new std::atomic_uint;
+    if (newPointer == nullptr){
+      counter = nullptr;
+    } else {
+      counter = new std::atomic_uint ;
       *counter=1;
     }
   };
@@ -31,37 +29,37 @@ class SharedPtr {
     if (sharedPtr.pointer){
       pointer = sharedPtr.pointer;
       counter = sharedPtr.counter;
-      if (pointer){
+      if  (pointer){
         *counter = *counter+1;
-      }
-      else{
+      } else {
         counter = nullptr;
       }
     }
   };
 
+  SharedPtr(SharedPtr&& sharedPtr) {
+    pointer = nullptr;
+    counter = nullptr;
+    if (sharedPtr.pointer) {
+      std::swap(pointer, sharedPtr.pointer);
+      std::swap(counter, sharedPtr.counter);
+    }
+  };
+
   SharedPtr& operator=(const SharedPtr& sharedPtr){
-    if (this != &sharedPtr){
+    if (this != &sharedPtr) {
       pointer = sharedPtr.pointer;
       counter = sharedPtr.counter;
-      if (pointer){
-        *counter = *counter-1;
-      }
-      else{
-        counter=nullptr;
+      if  (pointer){
+        *counter = *counter+1;
+      } else {
+        counter = nullptr;
       }
     }
     return *this;
   };
 
-  SharedPtr(SharedPtr&& sharedPtr){
-    if (sharedPtr.pointer) {
-      pointer = move(sharedPtr.pointer);
-      counter = move(sharedPtr.counter);
-    }
-  };
-
-  SharedPtr& operator=(SharedPtr&& sharedPtr){
+  SharedPtr& operator=(SharedPtr&& sharedPtr) {
     if (this != &sharedPtr) {
       std::swap(pointer, sharedPtr.pointer);
       std::swap(counter, sharedPtr.counter);
@@ -69,26 +67,61 @@ class SharedPtr {
     return *this;
   };
 
-  operator bool() const{
-    if(pointer){
-      return true;
+  ~SharedPtr() {
+    if (counter) {
+      if (!--*counter) {
+        delete pointer;
+        delete counter;
+      }
     }
-    else{
+  }
+
+  operator bool() const{
+    if(pointer)
+      return true;
+    else {
       return false;
     }
   }
 
-  T& operator*() const{
+  T& operator*() const {
     return *pointer;
-  };
-
-  T* get() {
-    return pointer;
   }
 
   T* operator->() const{
     return pointer;
   }
+
+  T* get() {
+    return pointer;
+  }
+
+  void reset() {
+    if(*counter==1){
+      delete pointer;
+      delete counter;
+    }else{
+      *counter=*counter-1;
+    }
+    pointer=nullptr;
+    counter=nullptr;
+  }
+
+  void reset(T* point){
+    if(*counter==1){
+      delete pointer;
+      delete counter;
+    }else {
+      *counter = *counter - 1;
+    }
+    pointer=point;
+    if(point==nullptr){
+      counter=nullptr;
+    }else{
+      counter=new atomic_uint;
+      *counter=1;
+    }
+  };
 
   void swap(SharedPtr& sharedPtr){
     T* tmpPointer = sharedPtr.pointer;
@@ -98,44 +131,6 @@ class SharedPtr {
     sharedPtr.counter = counter;
     counter = tmpCounter;
   };
-
-  void reset() {
-    if (*counter==1) {
-      delete pointer;
-      delete counter;
-    } else {
-      *counter = *counter - 1;
-    }
-    pointer=nullptr;
-    counter=nullptr;
-  };
-
-  void reset(T* anyPointer) {
-    if (*counter==1) {
-      delete pointer;
-      delete counter;
-    }
-    else {
-      *counter = *counter - 1;
-    }
-    pointer = anyPointer;
-    if (anyPointer==nullptr) {
-      counter = nullptr;
-    }
-    else{
-      counter=new atomic_uint;
-      *counter=1;
-    }
-  }
-
-  ~SharedPtr(){
-    if (counter) {
-      if (!--*counter){
-        delete pointer;
-        delete counter;
-      }
-    }
-  }
 
   [[nodiscard]] size_t use_count() const{
     if(counter){
@@ -149,4 +144,5 @@ class SharedPtr {
   T* pointer;
   atomic_uint* counter;
 };
+
 #endif // INCLUDE_HEADER_HPP_
--- tests/test.cpp
+++ tests/test.cpp
@@ -10,129 +10,129 @@ class TestClass {
 };
 
 TEST(testConstructor, pointNullptr) {
-SharedPtr <int> pointer;
-int* tmp = pointer.get();
-EXPECT_EQ(tmp, nullptr);
+  SharedPtr <int> pointer;
+  int* tmp = pointer.get();
+  EXPECT_EQ(tmp, nullptr);
 }
 
-TEST(testConstructor, TestMove){
-  int* anyPointer = new int(25);
-  SharedPtr <int> pointer(anyPointer);
-  SharedPtr <int> sPointer(move(pointer));
-  SharedPtr <int> thPointer;
-  thPointer = std::move(pointer);
-  SharedPtr <int> foPointer;
-  foPointer = thPointer;
-  EXPECT_EQ(25, *sPointer.get());
-  EXPECT_FALSE(pointer);
-  delete anyPointer;
-}
-
-TEST(testOperator, arrowTest) {
-auto testClassPointer = new TestClass;
-SharedPtr <TestClass> pointer(testClassPointer);
-testClassPointer -> a = 25;
-EXPECT_EQ(pointer -> a, testClassPointer -> a);
-}
-
-TEST(testOperator, StarTest) {
-int* anyPointer = new int (25);
-SharedPtr <int> pointer(anyPointer);
-EXPECT_EQ(25, *pointer);
-}
-TEST(testOperator, equalityLvalueTest) {
-  int* anyPointer = new int (25);
-  SharedPtr <int> pointer(anyPointer);
-  SharedPtr <int> sPointer = pointer;
-  SharedPtr <int> thPointer = pointer;
-  EXPECT_EQ(thPointer.get(), pointer.get());
-}
-
-TEST(testOperator, useCount){
-  int* anyPointer = new int (25);
-  SharedPtr <int> pointer(anyPointer);
-  SharedPtr <int> sPointer = pointer;
-  EXPECT_EQ(pointer.use_count(),2);
-}
-
-TEST(testOperator, boolTest) {
-  int* anyPointer = new int (25);
-  SharedPtr <int> pointer(anyPointer);
-  SharedPtr <int> sPointer = pointer;
-  bool isPointer = sPointer;
+TEST(testOperator, boolTrueTest) {
+  int* points = new int (25);
+  SharedPtr <int> pointer(points);
+  SharedPtr <int> pointer2 = pointer;
+  bool isPointer = pointer2;
   EXPECT_EQ(isPointer, true);
 }
 
-TEST(testMethod, GetTest) {
-int* anyPointer = nullptr;
-SharedPtr <int> pointer(anyPointer);
-int* tmp = pointer.get();
-EXPECT_EQ(tmp, nullptr);
+TEST(testOperator, arrowTest) {
+  auto testClassPointer = new TestClass;
+  testClassPointer -> a = 25;
+  SharedPtr <TestClass> pointer(testClassPointer);
+  EXPECT_EQ(pointer -> a, testClassPointer -> a);
 }
 
-TEST(testMethod, GetIntTest) {
-int* anyPointer = new int (25);
-SharedPtr <int> pointer(anyPointer);
-SharedPtr <int> sPointer = pointer;
-EXPECT_EQ(pointer.get(), anyPointer);
+TEST(testOperator, TestAsterisk) {
+  int* points = new int (25);
+  SharedPtr <int> pointer(points);
+  EXPECT_EQ(25, *pointer);
 }
 
-TEST(testMethod, ResetTest) {
-int* anyPointer = new int (25);
-SharedPtr <int> pointer(anyPointer);
-pointer.reset();
-EXPECT_EQ(pointer.get(), nullptr);
+TEST(testMethod, testGet) {
+  int* points = nullptr;
+  SharedPtr <int> pointer(points);
+  int* tmp = pointer.get();
+  EXPECT_EQ(tmp, nullptr);
 }
 
-TEST(testMethod, ResetTestElse){
-int* anyPointer = new int (25);
-SharedPtr <int> pointer(anyPointer);
-SharedPtr <int> sPointer = pointer;
-pointer.reset();
-EXPECT_EQ(pointer.get(), nullptr);
+TEST(testMethod, testGetInt) {
+  int* points = new int (25);
+  SharedPtr <int> pointer(points);
+  SharedPtr <int> pointer2 = pointer;
+  EXPECT_EQ(pointer.get(), points);
+}
+
+TEST(testMethod, resetTest) {
+  int* points = new int (25);
+  SharedPtr <int> pointer(points);
+  pointer.reset();
+  EXPECT_EQ(pointer.get(), nullptr);
+}
+
+TEST(testMethod, resetTestElse){
+  int* points = new int (25);
+  SharedPtr <int> pointer(points);
+  SharedPtr <int> pointer2 = pointer;
+  pointer.reset();
+  EXPECT_EQ(pointer.get(), nullptr);
 }
 
 TEST (testMethod, resetTestPoint) {
-int* anyPointer = new int (25);
-int* anotherPointer = new int (20);
-SharedPtr <int> pointer(anyPointer);
-pointer.reset(anotherPointer);
-EXPECT_EQ(pointer.get(), anotherPointer);
+  int* points = new int (25);
+  int* points1 = new int (20);
+  SharedPtr <int> pointer(points);
+  pointer.reset(points1);
+  EXPECT_EQ(pointer.get(), points1);
 }
 
 TEST (testMethod, resetTestPointElse) {
-int* anyPointer = new int (25);
-int* anotherPointer = new int (20);
-SharedPtr <int> pointer(anyPointer);
-SharedPtr <int> sPointer = pointer;
-pointer.reset(anotherPointer);
-EXPECT_EQ(pointer.get(), anotherPointer);
+  int* points = new int (25);
+  int* points1 = new int (20);
+  SharedPtr <int> pointer(points);
+  SharedPtr <int> pointer2 = pointer;
+  pointer.reset(points1);
+  EXPECT_EQ(pointer.get(), points1);
 }
 
 TEST(testMethod, swapTest) {
-int* anyPointer = new int (25);
-int* anotherPointer = new int (20);
-SharedPtr <int> pointer(anyPointer);
-SharedPtr <int> sPointer(anotherPointer);
-pointer.swap(sPointer);
-EXPECT_EQ(pointer.get(), anotherPointer);
-EXPECT_EQ(sPointer.get(), anyPointer);
+  int* points = new int (25);
+  int* points1 = new int (20);
+  SharedPtr <int> pointer(points);
+  SharedPtr <int> pointer1(points1);
+  pointer.swap(pointer1);
+  EXPECT_EQ(pointer.get(), points1);
+  EXPECT_EQ(pointer1.get(), points);
+}
+
+TEST(testOperator, equalityLvalueTest) {
+  int* points = new int (25);
+  SharedPtr <int> pointer(points);
+  SharedPtr <int> pointer2 = pointer;
+  SharedPtr <int> pointer3 = pointer2;
+  EXPECT_EQ(pointer3.get(), pointer.get());
+}
+
+TEST(testOperator, useCount){
+  int* points = new int (25);
+  SharedPtr <int> pointer(points);
+  SharedPtr <int> pointer2 = pointer;
+  EXPECT_EQ(pointer.use_count(),2);
+}
+
+TEST(testConstructor,Move){
+  int* points = new int (25);
+  SharedPtr <int> pointer(points);
+  SharedPtr <int> pointer1(move(pointer));
+  SharedPtr <int> pointer3;
+  pointer3 = std::move(pointer);
+  SharedPtr <int> pointer4;
+  pointer4 = pointer3;
+  EXPECT_EQ(25, *pointer1.get());
+  EXPECT_FALSE(pointer);
 }
 
 TEST(testIsMove, assignConstTest) {
-EXPECT_EQ(std::is_move_assignable<SharedPtr<int>>::value, true);
-EXPECT_EQ(std::is_move_constructible<SharedPtr<int>>::value, true);
-EXPECT_EQ(std::is_move_assignable<SharedPtr<double>>::value, true);
-EXPECT_EQ(std::is_move_constructible<SharedPtr<double>>::value,true);
-EXPECT_EQ(std::is_move_assignable<SharedPtr<TestClass>>::value,true);
-EXPECT_EQ(std::is_move_constructible<SharedPtr<TestClass>>::value,true);
+  EXPECT_EQ(std::is_move_assignable<SharedPtr<int>>::value, true);
+  EXPECT_EQ(std::is_move_constructible<SharedPtr<int>>::value, true);
+  EXPECT_EQ(std::is_move_assignable<SharedPtr<double>>::value, true);
+  EXPECT_EQ(std::is_move_constructible<SharedPtr<double>>::value,true);
+  EXPECT_EQ(std::is_move_assignable<SharedPtr<TestClass>>::value,true);
+  EXPECT_EQ(std::is_move_constructible<SharedPtr<TestClass>>::value,true);
 }
 
 TEST(testIsCopy, assignConstTest) {
-EXPECT_EQ(std::is_copy_assignable<SharedPtr<int>>::value, true);
-EXPECT_EQ(std::is_copy_constructible<SharedPtr<int>>::value, true);
-EXPECT_EQ(std::is_copy_assignable<SharedPtr<double>>::value, true);
-EXPECT_EQ(std::is_copy_constructible<SharedPtr<double>>::value,true);
-EXPECT_EQ(std::is_copy_assignable<SharedPtr<TestClass>>::value,true);
-EXPECT_EQ(std::is_copy_constructible<SharedPtr<TestClass>>::value,true);
+  EXPECT_EQ(std::is_copy_assignable<SharedPtr<int>>::value, true);
+  EXPECT_EQ(std::is_copy_constructible<SharedPtr<int>>::value, true);
+  EXPECT_EQ(std::is_copy_assignable<SharedPtr<double>>::value, true);
+  EXPECT_EQ(std::is_copy_constructible<SharedPtr<double>>::value,true);
+  EXPECT_EQ(std::is_copy_assignable<SharedPtr<TestClass>>::value,true);
+  EXPECT_EQ(std::is_copy_constructible<SharedPtr<TestClass>>::value,true);
 }
